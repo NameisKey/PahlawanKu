f1e8b059c5bfff6520ddaf488b3a6c02661e81e2
NameisKey <106938020+NameisKey@users.noreply.github.com>
2022-11-21 14:34:28 +0700

    finis?

--- app/src/main/java/com/example/pahlawanku/AdapterCard.java
+++ app/src/main/java/com/example/pahlawanku/AdapterCard.java
@@ -1,5 +1,7 @@
 package com.example.pahlawanku;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +17,11 @@ import java.util.ArrayList;
 
 public class AdapterCard extends RecyclerView.Adapter<AdapterCard.ClassViewHolder>{
     private ArrayList<ModelPahlawan> dataPahlawan;
+    private Context ctx;
 
-    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan) {
+    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan, Context ctx) {
         this.dataPahlawan = dataPahlawan;
+        this.ctx = ctx;
     }
 
     @NonNull
@@ -33,10 +37,26 @@ public class AdapterCard extends RecyclerView.Adapter<AdapterCard.ClassViewHolde
         holder.tvNama.setText(pahlawan.getNama());
         holder.tvTentang.setText(pahlawan.getTentang());
         Glide
-                .with(holder.itemView.getContext())
+                .with(ctx)
                 .load(pahlawan.getFoto())
                 .centerCrop()
                 .into(holder.ivFoto);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String xNama, xTentang, xFoto;
+
+                xNama = pahlawan.getNama();
+                xTentang = pahlawan.getTentang();
+                xFoto = pahlawan.getFoto();
+
+                Intent kirim = new Intent(ctx(),DetailActivity.class);
+                kirim.putExtra("xNama", xNama);
+                kirim.putExtra("xTentang", xTentang);
+                kirim.putExtra("xFoto",xFoto);
+                ctx.startActivity(kirim);
+            }
+        });
 
     }
 
@@ -54,6 +74,9 @@ public class AdapterCard extends RecyclerView.Adapter<AdapterCard.ClassViewHolde
             tvNama = itemView.findViewById(R.id.tv_nama);
             tvTentang = itemView.findViewById(R.id.tv_tentang);
             ivFoto = itemView.findViewById(R.id.iv_foto);
+
+
+
         }
     }
 }
